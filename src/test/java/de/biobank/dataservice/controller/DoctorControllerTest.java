419e76cdda252f7f3e5d3ad788247a9c54ce81c8
Dennis Wilmsmann <denniswilmsmann@gmx.de>
2021-03-06 13:06:17 +0100

    docker build fixed

--- src/test/java/de/biobank/dataservice/controller/DoctorControllerTest.java
+++ src/test/java/de/biobank/dataservice/controller/DoctorControllerTest.java
@@ -1,15 +1,22 @@
 package de.biobank.dataservice.controller;
 
+import de.biobank.dataservice.entity.Doctor;
+import de.biobank.dataservice.entity.Profession;
+import de.biobank.dataservice.repository.DoctorRepository;
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,6 +25,9 @@ public class DoctorControllerTest {
     @LocalServerPort
     private int port;
 
+    @MockBean
+    private DoctorRepository doctorRepository;
+
     @BeforeEach
     public void setup() {
         RestAssured.port = this.port;
@@ -25,6 +35,15 @@ public class DoctorControllerTest {
 
     @Test
     void getDoctor() {
+        when(doctorRepository.findById(42L)).thenReturn(Optional.of(Doctor.builder()
+                .id(42L)
+                .firstName("Max")
+                .lastName("Musterarzt")
+                .zip("12345")
+                .city("Musterhausen")
+                .profession(Profession.ALLGEMEINMEDIZIN)
+                .build()));
+
         given().log().all()
                 .when().get("/doctor/42")
                 .then().log().all()
